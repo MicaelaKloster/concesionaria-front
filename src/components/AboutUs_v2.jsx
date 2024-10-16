8bec36776c166a087877e2e37ac20c88727e3e5e
Micaela Kloster <108692944+MicaelaKloster@users.noreply.github.com>
2024-10-16 20:41:11 -0300

    Actualización de las cards

--- src/components/AboutUs_v2.jsx
+++ src/components/AboutUs_v2.jsx
@@ -6,19 +6,19 @@
         <path stroke-linecap="round" stroke-linejoin="round" d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418" />
       </svg>
       <h5 class="ml-3 text-slate-800 text-xl font-semibold">
-        Website Review Check
+        Encontra el auto perfecto para vos
       </h5>
     </div>
     <p class="block text-slate-600 leading-normal font-light mb-4">
-      Because it&apos;s about motivating the doers. Because I&apos;m here to follow my dreams and inspire others.
+      En nuestro concesionario, nos especializamos en ofrecer una amplia selección de autos de las mejores marcas, adaptados a tus necesidades y presupuesto. Desde modelos deportivos hasta familiares, contamos con opciones para todos los gustos. Nuestro equipo de ventas está listo para asesorarte en cada paso del proceso, brindándote una experiencia de compra personalizada y sin complicaciones.
     </p>
     <div>
-      <a href="#" class="text-slate-800 font-semibold text-sm hover:underline flex items-center">
+{/*       <a href="#" class="text-slate-800 font-semibold text-sm hover:underline flex items-center">
         Learn More
         <svg xmlns="http://www.w3.org/2000/svg" class="ml-2 h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
         </svg>
-      </a>
+      </a> */}
     </div>
   </div>
   
@@ -29,19 +29,19 @@
         <path stroke-linecap="round" stroke-linejoin="round" d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418" />
       </svg>
       <h5 class="ml-3 text-slate-800 text-xl font-semibold">
-        Website Review Check
+        Cuidamos tu auto, siempre
       </h5>
     </div>
     <p class="block text-slate-600 leading-normal font-light mb-4">
-      Because it&apos;s about motivating the doers. Because I&apos;m here to follow my dreams and inspire others.
+      Sabemos lo importante que es mantener tu vehículo en óptimas condiciones. Por eso, ofrecemos un completo servicio de reparaciones y mantenimiento, respaldado por garantías que te brindan tranquilidad. Nuestro equipo de técnicos especializados está capacitado para realizar desde simples revisiones hasta reparaciones complejas, asegurando que tu auto esté siempre listo para rodar.
     </p>
     <div>
-      <a href="#" class="text-slate-800 font-semibold text-sm hover:underline flex items-center">
+{/*       <a href="#" class="text-slate-800 font-semibold text-sm hover:underline flex items-center">
         Learn More
         <svg xmlns="http://www.w3.org/2000/svg" class="ml-2 h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
         </svg>
-      </a>
+      </a> */}
     </div>
   </div>
   
@@ -52,19 +52,19 @@
         <path stroke-linecap="round" stroke-linejoin="round" d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418" />
       </svg>
       <h5 class="ml-3 text-slate-800 text-xl font-semibold">
-        Website Review Check
+        Financiación a tu medida
       </h5>
     </div>
     <p class="block text-slate-600 leading-normal font-light mb-4">
-      Because it&apos;s about motivating the doers. Because I&apos;m here to follow my dreams and inspire others.
+      Con nosotros, el auto de tus sueños está más cerca de lo que pensás. Ofrecemos planes de financiamiento flexibles y adaptados a tus posibilidades, con tasas competitivas y opciones de pago accesibles. Nuestro equipo de asesores financieros te acompañará para que encuentres el plan que mejor se ajuste a tu presupuesto, haciendo que la compra de tu auto sea más fácil y conveniente.
     </p>
     <div>
-      <a href="#" class="text-slate-800 font-semibold text-sm hover:underline flex items-center">
+{/*       <a href="#" class="text-slate-800 font-semibold text-sm hover:underline flex items-center">
         Learn More
         <svg xmlns="http://www.w3.org/2000/svg" class="ml-2 h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
         </svg>
-      </a>
+      </a> */}
     </div>
   </div>
 </div>
