2c8c430d502f0ca0e0789147a21137a0a81709f1
Micaela Kloster <108692944+MicaelaKloster@users.noreply.github.com>
2024-10-17 11:16:27 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import Feedback from  './components/Feedback';
-import AboutUs from './components/AboutUs';
-import Footer from './components/Footer';
+import Navbar from './components/Navbar_v2';
+import Home from './components/Home_v2';
+import Feedback from  './components/Feedback_v2';
+import AboutUs from './components/AboutUs_v2';
+import Footer from './components/Footer_v2';
 
 function App() {
   return (
